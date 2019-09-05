6d514e0a06f1fa6efaa6f6ba14216f86a516aa66
mohana gowthami <mohanagajula579@gmail.com>
2019-09-05 15:55:07 +0530

    code editor assign4 getting error file not found

---
+++ App.js
@@ -0,0 +1,50 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import {WebView} from 'react-native-webview';
+
+const App = () => {
+  const sourceUri =
+    (Platform.OS === 'android' ? 'file:///android_asset/' : '') +
+    'html/Web.bundle/loader.html';
+  const params = 'platform=' + Platform.OS;
+  const injectedJS = `
+    if (!window.location.search) {
+      var link = document.getElementById('progress-bar');
+
+      
+      link.href = './site/index.html?${params}';
+
+      link.click();
+    }
+  `;
+
+  return (
+    <Fragment>
+      <Text>gowthami</Text>
+      <WebView
+        injectedJavaScript={injectedJS}
+        source={{uri: sourceUri}}
+        javaScriptEnabled={true}
+        originWhitelist={['*']}
+        allowFileAccess={true}
+      />
+    </Fragment>
+  );
+};
+
+export default App;
